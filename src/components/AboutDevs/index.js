f6b74ce9070c5188342048b6a84e20b9d84259d5
SiimonStark <justinpyktel@gmail.com>
2019-08-13 04:12:58 -0600

    Whole card links to dev's Github, remove green border

--- src/components/AboutDevs/index.js
+++ src/components/AboutDevs/index.js
@@ -4,24 +4,28 @@ const AboutDevs = () => {
   const devs = [
     {
       name: "Jon Peterson",
+      gH: "https://github.com/joequincy",
       role: "BE",
       summary:
         "A tinkerer by nature, I enjoy hacking away on an interesting idea and learning new tricks along the way. I’m always looking for a new idea or technology to explore!"
     },
     {
       name: "Justin Pyktel",
+      gH: "https://github.com/SiimonStark",
       role: "FE",
       summary:
         "TBD"
     },
     {
       name: "Lynne Rang",
+      gH: "https://github.com/lynnerang",
       role: "FE",
       summary:
         "TBD"
     },
     {
       name: "Rachael Drennan",
+      gH: "https://github.com/rdren0",
       role: "FE/BE",
       summary:
         "TBD"
@@ -43,15 +47,14 @@ const AboutDevs = () => {
 
     return devs.map(dev => {
       return (
-        <article className="dev" key={dev.name}>
-          <div className="dev-pic">
-            
-          </div>
-          <h3>{dev.name}</h3>
+        <a className="dev" href={dev.gH} target="blank" key={dev.name}>
+          <div className="dev-pic" />
+          <h4>{dev.name}</h4>
           <h5>Role: {dev.role}</h5>
+          <div className="divider" />
           <p>{dev.summary}</p>
-        </article>
-      )
+        </a>
+      );
     })
   }
 
