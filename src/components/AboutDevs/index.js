52168da4bba4b45998f24306d83eb06951e78308
SiimonStark <justinpyktel@gmail.com>
2019-08-14 22:23:45 -0600

    Added [Description, Links, Styling]

--- src/components/AboutDevs/index.js
+++ src/components/AboutDevs/index.js
@@ -1,34 +1,51 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react';
 
 const AboutDevs = (props) => {
   const devs = [
     {
       name: "Jon Peterson",
-      gH: "https://github.com/joequincy",
+      gH_Link: "https://github.com/joequincy",
+      l_Link: "https://www.linkedin.com/in/joequincy/",
+      p_Link: "https://alumni.turing.io/alumni/jon-peterson",
       role: "BE",
+      img:
+        "https://media.licdn.com/dms/image/C4E03AQEhILN-5s-jEw/profile-displayphoto-shrink_800_800/0?e=1571270400&v=beta&t=WBZ4ztxssw3W-PwaLADES30A7HvX-4anP6oPfkRLpYs",
       summary:
         "A tinkerer by nature, I enjoy hacking away on an interesting idea and learning new tricks along the way. I’m always looking for a new idea or technology to explore!"
     },
     {
       name: "Justin Pyktel",
-      gH: "https://github.com/SiimonStark",
+      gH_Link: "https://github.com/SiimonStark",
+      l_Link: "https://www.linkedin.com/in/justinpyktel/",
+      p_Link: "https://alumni.turing.io/alumni/justin-pyktel",
       role: "FE",
+      img:
+        "https://media.licdn.com/dms/image/C4E03AQGFJ9zu6O-7Zw/profile-displayphoto-shrink_200_200/0?e=1571270400&v=beta&t=KmObYjlcIRPiKy0wB2itsWgIJAwBNVyHPKoXlobY_0o",
       summary:
-        "TBD"
+        "Transitioning into the tech field I intend to keep my audience at the forefront of design choices. Creating a more enjoyable and fun experience is certainly a win win!"
     },
     {
       name: "Lynne Rang",
-      gH: "https://github.com/lynnerang",
+      gH_Link: "https://github.com/lynnerang",
+      l_Link: "https://www.linkedin.com/in/lynne-rang/",
+      p_Link: "https://alumni.turing.io/alumni/lynne-rang",
       role: "FE",
+      img:
+        "https://media.licdn.com/dms/image/C4E03AQFu8BraqA9MJA/profile-displayphoto-shrink_800_800/0?e=1571270400&v=beta&t=u4QsbYsdbK4CYp0MaKpv0I6Ky9jKs3h0b_lF_3VC8vY",
       summary:
-        "TBD"
+        "My skills as a developer come together with a holistic understanding of what it takes to make a great product"
     },
     {
       name: "Rachael Drennan",
-      gH: "https://github.com/rdren0",
+      gH_Link: "https://github.com/rdren0",
+      l_Link: "https://www.linkedin.com/in/rachael-drennan/",
+      p_Link: "https://alumni.turing.io/alumni/rachael-drennan",
       role: "FE/BE",
+      img:
+        "https://media.licdn.com/dms/image/C4E03AQFmgk9OS5Z88Q/profile-displayphoto-shrink_800_800/0?e=1571270400&v=beta&t=A8lyTbvCZ7IxW1ctg19Oqw0EfVq06xUh1kMK14vybSI",
       summary:
-        "TBD"
+        "As an artist and problem solver, I was drawn to software engineering because it allows me to be both creative and analytical in my approach to a project."
     }
   ];
 
@@ -47,23 +64,51 @@ const AboutDevs = (props) => {
 
     return devs.map(dev => {
       return (
-        <a className="dev" href={dev.gH} target="blank" key={dev.name}>
-          <div className="dev-pic" />
+        <div className="dev" key={dev.name}>
+          <div className="dev-pic">
+            <img
+              className="dev-img"
+              src={dev.img}
+              alt={`Pic of Dev: ${dev.name}`}
+            />
+          </div>
           <h4>{dev.name}</h4>
           <h5>Role: {dev.role}</h5>
           <div className="divider" />
           <p>{dev.summary}</p>
-        </a>
+          <div className="social">
+            <a
+              className="fab fa-linkedin"
+              href={dev.l_Link}
+              target="blank"
+            />
+            <a
+              className="fab fa-github-square"
+              href={dev.gH_Link}
+              target="blank"
+            />
+            <a
+              className="fas fa-address-card"
+              href={dev.p_Link}
+              target="blank"
+            />
+          </div>
+        </div>
       );
     })
   }
 
   return (
-    <div className="AboutDevs">
-      <i className="fas fa-arrow-alt-circle-left" 
-        onClick={() => props.history.goBack()} />
-      <h2>Top Secret Case Files:</h2>
-      <section className="devs">{renderDevs()}</section>
+    <div className="backdrop">
+      <div className="AboutDevs">
+        <i
+          id="back"
+          className="fas fa-arrow-alt-circle-left"
+          onClick={() => props.history.goBack()}
+        />
+        <h2>Top Secret Case Files:</h2>
+        <section className="devs">{renderDevs()}</section>
+      </div>
     </div>
   );
 }
